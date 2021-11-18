bdee75a7f688aae6628dd1568d37876c4fc0922c
Francesco <scovascova@gmail.com>
2021-11-18 18:29:04 +0100

    switch per i quadrati

--- js/script.js
+++ js/script.js
@@ -5,25 +5,48 @@ document.getElementById('play').addEventListener('click', function(){
 })
 
 function play(){
-   const livSelezionato = document.getElementById('level');
+   const livSelezionato = parseInt(document.getElementById('level').value);
+
+   let boxNumber;
+   let boxForSide;
+   
+   switch(livSelezionato){
+        case 1:
+           boxNumber = 100;
+           break;
+        case 2:
+           boxNumber = 81;
+           break;
+        case 3:
+            boxNumber = 49;
+   }
+
+   boxForSide = Math.sqrt(boxNumber);
+   
+   for(let i = 1; i <= boxNumber; i++)
 }
 
-function addBox(){
-    const nodo = document.createElement('div');
-    nodo.className ='box';
-    return nodo;
-}
 
-for(let i = 1; i <= 100; i++){
-    const divBox = addBox();
-    divBox.innerHTML = i;
-    container.appendChild(divBox);
-    divBox.addEventListener('click', function(){
-      divBox.classList.add('cliccato-true');
-      console.log('click');
-    });
+
+
+
+
+// function addBox(){
+//     const nodo = document.createElement('div');
+//     nodo.className ='box';
+//     return nodo;
+// }
+
+// for(let i = 1; i <= 100; i++){
+//     const divBox = addBox();
+//     divBox.innerHTML = i;
+//     container.appendChild(divBox);
+//     divBox.addEventListener('click', function(){
+//       divBox.classList.add('cliccato-true');
+//       console.log('click');
+//     });
     
-}
+// }
 
 
 
